e82f9b5dae90200939bc512dda53283f052c6665
Prajwal Halasahally KeshavaReddy <prajwal19@gmail.com>
2015-12-03 01:03:11 -0600

    work model added

--- src/main/java/utd/phk/web/model/Work.java
+++ src/main/java/utd/phk/web/model/Work.java
@@ -2,6 +2,18 @@ package utd.phk.web.model;
 
 public class Work {
 
-	private String 
+	private String workId;
+	private String createdBy;
+	private String assignStatus;
+	private String assignTo;
+	private String tsCreated;
+	private String tsAlloted;
+	private String tsExpiry;
+	private String tsCompleted;
+	private String compStatus;
+	private String startLocation;
+	private String endLocation;
+	private String cost;
+	
 
 }
