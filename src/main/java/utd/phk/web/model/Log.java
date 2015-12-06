62a062ee8b78e55b4610596a3689adb6e13afac4
Prajwal Halasahally KeshavaReddy <prajwal19@gmail.com>
2015-12-05 21:56:16 -0600

    Log Model creted

---
+++ src/main/java/utd/phk/web/model/Log.java
@@ -0,0 +1,43 @@
+package utd.phk.web.model;
+
+public class Log {
+
+	private String logId;
+	private String bidderId;
+	private String acceptorId;
+	private String workId;
+	private String workStatus;
+	public String getLogId() {
+		return logId;
+	}
+	public void setLogId(String logId) {
+		this.logId = logId;
+	}
+	public String getBidderId() {
+		return bidderId;
+	}
+	public void setBidderId(String bidderId) {
+		this.bidderId = bidderId;
+	}
+	public String getAcceptorId() {
+		return acceptorId;
+	}
+	public void setAcceptorId(String acceptorId) {
+		this.acceptorId = acceptorId;
+	}
+	public String getWorkId() {
+		return workId;
+	}
+	public void setWorkId(String workId) {
+		this.workId = workId;
+	}
+	public String getWorkStatus() {
+		return workStatus;
+	}
+	public void setWorkStatus(String workStatus) {
+		this.workStatus = workStatus;
+	}
+	
+	
+	
+}
